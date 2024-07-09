0c036a301ceb35d195fff6d3342b8330e38e7502
OldManKatan <78036601+OldManKatan@users.noreply.github.com>
2024-07-09 01:46:03 -0500

    Initial commit

---
+++ TamesFollow/TamesFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace TamesFollow
+{
+    [BepInPlugin(PluginId, "Tames Follow", "1.0.0")]
+    public class TamesFollow : BaseUnityPlugin
+    {
+        public const string PluginId = "oldmankatan.mods.tamesfollow";
+        private static TamesFollow _instance;
+
+        private Harmony _harmony;
+
+        [UsedImplicitly]
+        private void Awake()
+        {
+            _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginId);
+            _instance = this;
+        }
+
+        [UsedImplicitly]
+        private void OnDestroy()
+        {
+            _harmony?.UnpatchSelf();
+            _instance = null;
+        }
+    }
+
+    [HarmonyPatch(typeof(Tameable), "Interact")]
+    public class Tameable_Interact_Patch
+    {
+        private static void Prefix(Tameable __instance) {
+            Character m_character = __instance.GetComponent<Character>();
+            if (m_character.IsTamed())
+            {
+                __instance.m_commandable = true;
+            }
+        }
+    }
+}
